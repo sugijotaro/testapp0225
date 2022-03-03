a16b15c14c2c891eb4c48643c56e93518b5bae2a
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2022-03-03 10:54:57 +0900

    できたー！

--- lib/stream_macos/mac_os.dart
+++ lib/stream_macos/mac_os.dart
@@ -3,6 +3,8 @@ import 'package:testapp0225/stream_macos/mac_os_logic.dart';
 import "dart:async";
 import 'package:testapp0225/main.dart';
 
+double version = 0.0;
+
 void main() {
   runApp(MacOSApp());
 }
@@ -29,25 +31,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  var intStream = StreamController<int>();
+  // int _counter = 0;
+  var doubleStream = StreamController<double>();
 
   var stringStream = StreamController<String>.broadcast();
-  var generator = new Generator();
+  var receiver = new Receiver();
   var coodinator = new Coordinator();
   var consumer = new Consumer();
 
   void _incrementCounter() {
-    generator.generate();
+    receiver.receive(version);
     setState(() {
-      _counter++;
+      // _counter++;
     });
   }
 
   @override
   void initState() {
-    generator.init(intStream);
-    coodinator.init(intStream, stringStream);
+    receiver.init(doubleStream);
+    coodinator.init(doubleStream, stringStream);
     consumer.init(stringStream);
     coodinator.coorinate();
     consumer.consume();
@@ -57,7 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void dispose() {
-    intStream.close();
+    doubleStream.close();
     stringStream.close();
     super.dispose();
   }
@@ -72,42 +74,54 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              key: Key('counter'),
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            StreamBuilder<String>(
-              stream: stringStream.stream,
-              initialData: "",
-              builder: (context, snapshot) {
-                return Text(
-                  'RANDOM : ${snapshot.data}',
-                  style: Theme.of(context).textTheme.headline4,
-                );
-              },
-            ),
-            RaisedButton(
-              child: Text('戻る'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MyApp()),
-                );
-              },
-            ),
-          ],
+        TextField(
+          decoration: InputDecoration(
+          hintText: 'macOS（Mac OS X）バージョンを入力',
         ),
+        onChanged: (text) {
+          version = double.parse(text);
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        key: Key('increment'),
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            RaisedButton(
+              child: Text('決定'),
+              onPressed: () {
+                receiver.receive(version);
+              },
+            ),
+      StreamBuilder<String>(
+        stream: stringStream.stream,
+        initialData: "",
+        builder: (context, snapshot) {
+          return Text(
+            'Code Name : ${snapshot.data}',
+            style: Theme
+                .of(context)
+                .textTheme
+                .headline4,
+          );
+        },
+      ),
+      RaisedButton(
+        child: Text('戻る'),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => MyApp()),
+          );
+        },
+      ),
+      ],
+    ),
+    ),
+    // floatingActionButton: FloatingActionButton(
+    // key: Key('increment'),
+    // onPressed: _incrementCounter,
+    // tooltip: 'Increment',
+    // child: Icon(Icons.
+    // add
+    // )
+    // ,
+    // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/stream_macos/mac_os_logic.dart
+++ lib/stream_macos/mac_os_logic.dart
@@ -1,35 +1,58 @@
 import "dart:math" as math;
 import "dart:async";
 
-class Generator {
+Map<double, String> list = {
+  10.0 : "Cheetah",
+  10.1 : "Puma",
+  10.2 : "Jaguar",
+  10.3 : "Panther",
+  10.4 : "Tiger",
+  10.5 : "Leopard",
+  10.6 : "Snow Leopard",
+  10.7 : "Lion",
+  10.8 : "Mountain Lion",
+  10.9 : "Mavericks",
+  10.10 : "Yosemite",
+  10.11 : "El Capitan",
+  10.12 : "Sierra",
+  10.13 : "High Sierra",
+  10.14 : "Mojave",
+  10.15 : "Catalina",
+  11.0 : "Big Sur",
+  12.0 : "Monterey"
+};
+
+class Receiver {
   var rand;
-  var intStream;
-  init(StreamController<int> stream) {
+  var doubleStream;
+
+  init(StreamController<double> stream) {
     rand = new math.Random();
-    intStream = stream;
+    doubleStream = stream;
   }
 
   // ランダムな整数を作る
-  generate() {
-    var data = rand.nextInt(100);
-    print("generatorが$dataを作ったよ");
-    intStream.sink.add(data);
+  receive(double data) {
+    print("receiverが$dataを受け取ったよ");
+    doubleStream.sink.add(data);
   }
 }
 
 class Coordinator {
-  var intStream;
+  var doubleStream;
   var strStream;
-  init(StreamController<int> intStream,
+
+  init(StreamController<double> doubleStream,
       StreamController<String> strStream) {
-    this.intStream = intStream;
+    this.doubleStream = doubleStream;
     this.strStream = strStream;
   }
 
-  // 流れてきたものをintからStringにする
+  // 流れてきたものをdoubleからStringにする
   coorinate() {
-    intStream.stream.listen((data) async {
-      String newData = data.toString();
+    doubleStream.stream.listen((data) async {
+      // String newData = list[list.indexOf(data)][1];
+      String newData = list[data] ?? "そんなバージョンはない";
       print("coordinatorが$dataから$newDataに変換したよ");
       strStream.sink.add(newData);
     });
@@ -38,6 +61,7 @@ class Coordinator {
 
 class Consumer {
   var strStream;
+
   init(StreamController<String> stream) {
     strStream = stream;
   }
